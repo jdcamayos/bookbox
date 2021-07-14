61ba2a260704ccbd1cd209d7c924da7cb6fe5576
jdcamayos <jd.camayo@gmail.com>
2021-07-14 18:18:35 -0500

    added morgan middleware for development mode

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,11 +3,17 @@ const app = express()
 
 const { config } = require('./config/index')
 
+// Importing Middlewares
+const morgan = require('morgan')
+
 // Importing Routes
 const booksApi = require('./routes/books.routes')
 
 // Body parser
 app.use(express.json())
+
+// Morgan 
+app.use(morgan('dev'))
 
 // Routes
 booksApi(app)
