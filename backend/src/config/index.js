2befe1207f9c8dc29bebbec790c7149404d4d9de
jdcamayos <jd.camayo@gmail.com>
2021-07-14 18:13:32 -0500

    updated variables environment structure

--- backend/src/config/index.js
+++ backend/src/config/index.js
@@ -1,42 +1,43 @@
 require('dotenv').config()
 
-const enviroment = process.env.NODE_ENV === 'production'
+const dev = !(process.env.NODE_ENV === 'production')
+console.log(`Running in ${dev ? 'Development' : 'Production'} mode`)
 
 const config = {
-    dev: process.env.NODE_ENV === 'production',
-    port: enviroment 
-        ? process.env.PORT
-        : process.env.DEV_PORT || 4000,
-    cors: enviroment
-        ? process.env.CORS
-        : process.env.DEV_CORS,
-    dbUser: enviroment
-        ? process.env.DB_USER
-        : process.env.DEV_DB_USER,
-    dbPassword: enviroment
-        ? process.env.DB_PASSWORD
-        : process.env.DEV_DB_PASSWORD,
-    dbHost: enviroment
-        ? process.env.DB_HOST
-        : process.env.DEV_DB_HOST,
-    dbName: enviroment
-        ? process.env.DB_NAME
-        : process.env.DEV_DB_NAME,
-    defaultAdminPassword: enviroment
-        ? process.env.DEFAULT_ADMIN_PASSWORD
-        : process.env.DEV_DEFAULT_ADMIN_PASSWORD,
-    defaultUserPassword: enviroment
-        ? process.env.DEFAULT_USER_PASSWORD
-        : process.env.DEV_DEFAULT_USER_PASSWORD,
-    authJwtSecret: enviroment
-        ? process.env.AUTH_JWT_SECRET
-        : process.env.DEV_AUTH_JWT_SECRET,
-    publicApiKeyToken: enviroment
-        ? process.env.PUBLIC_API_KEY_TOKEN
-        : process.env.DEV_PUBLIC_API_KEY_TOKEN,
-    adminApiKeyToken: enviroment
-        ? process.env.ADMIN_API_KEY_TOKEN
-        : process.env.DEV_ADMIN_API_KEY_TOKEN,
+    dev,
+    port: dev 
+    ? process.env.DEV_PORT || 4000
+    : process.env.PORT,
+    cors: dev
+    ? process.env.DEV_CORS
+    : process.env.CORS,
+    dbUser: dev
+    ? process.env.DEV_DB_USER
+    : process.env.DB_USER,
+    dbPassword: dev
+    ? process.env.DEV_DB_PASSWORD
+    : process.env.DB_PASSWORD,
+    dbHost: dev
+    ? process.env.DEV_DB_HOST
+    : process.env.DB_HOST,
+    dbName: dev
+    ? process.env.DEV_DB_NAME
+    : process.env.DB_NAME,
+    defaultAdminPassword: dev
+    ? process.env.DEV_DEFAULT_ADMIN_PASSWORD
+    : process.env.DEFAULT_ADMIN_PASSWORD,
+    defaultUserPassword: dev
+    ? process.env.DEV_DEFAULT_USER_PASSWORD
+    : process.env.DEFAULT_USER_PASSWORD,
+    authJwtSecret: dev
+    ? process.env.DEV_AUTH_JWT_SECRET
+    : process.env.AUTH_JWT_SECRET,
+    publicApiKeyToken: dev
+    ? process.env.DEV_PUBLIC_API_KEY_TOKEN
+    : process.env.PUBLIC_API_KEY_TOKEN,
+    adminApiKeyToken: dev
+    ? process.env.DEV_ADMIN_API_KEY_TOKEN
+    : process.env.ADMIN_API_KEY_TOKEN,
 }
 
 module.exports = { config }
