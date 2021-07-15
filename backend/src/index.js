b52dcc0c6c4a57d5de804490151a19ffc7092a6b
jdcamayos <jd.camayo@gmail.com>
2021-07-14 22:36:49 -0500

    added error handler middleware and schema verification in routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ const { config } = require('./config/index')
 
 // Importing Middlewares
 const morgan = require('morgan')
+const { logErrors, wrapErrors, errorHandler } = require('./utils/middlewares/errorHandler')
 
 // Importing Routes
 const booksApi = require('./routes/books.routes')
@@ -17,6 +18,11 @@ app.use(morgan('dev'))
 
 // Routes
 booksApi(app)
+
+// Errors Middlewares
+app.use(logErrors)
+app.use(wrapErrors)
+app.use(errorHandler)
 
 app.listen(config.port, function () {
     console.log(`Server listening on http://localhost:${config.port}`)
--- backend/src/routes/books.routes.js
+++ backend/src/routes/books.routes.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const BooksService = require('../services/books.service')
+const validationHandler = require('../utils/middlewares/validationHandler')
+
+// Schemas
+const {
+    bookIdSchema,
+    createBookSchema,
+    updateBookSchema,
+} = require('../utils/schemas/books')
 
 function booksApi(app) {
     const router = express.Router()
@@ -20,63 +28,80 @@ function booksApi(app) {
         }
     })
 
-    router.get('/:bookId', async function (req, res, next) {
-        const { bookId } = req.params
-        try {
-            const book = await booksService.getBook({ bookId })
-            res.status(200).json({
-                data: book,
-                message: 'book retrieved',
-            })
-        } catch (error) {
-            next(error)
+    router.get(
+        '/:bookId',
+        validationHandler({ bookId: bookIdSchema }),
+        async function (req, res, next) {
+            const { bookId } = req.params
+            try {
+                const book = await booksService.getBook({ bookId })
+                res.status(200).json({
+                    data: book,
+                    message: 'book retrieved',
+                })
+            } catch (error) {
+                next(error)
+            }
         }
-    })
+    )
 
-    router.post('/', async function (req, res, next) {
-        const { body: book } = req
-        try {
-            const createdBookId = await booksService.createBook({ book })
-            res.status(201).json({
-                data: createdBookId,
-                message: 'book created',
-            })
-        } catch (error) {
-            next(error)
+    router.post(
+        '/',
+        validationHandler(createBookSchema),
+        async function (req, res, next) {
+            const { body: book } = req
+            try {
+                const createdBookId = await booksService.createBook({ book })
+                res.status(201).json({
+                    data: createdBookId,
+                    message: 'book created',
+                })
+            } catch (error) {
+                next(error)
+            }
         }
-    })
+    )
 
-    router.put('/:bookId', async function (req, res, next) {
-        const { bookId } = req.params
-        const { body: book } = req
-        try {
-            const updatedBookId = await booksService.updateBook({
-                bookId,
-                book,
-            })
-            res.status(200).json({
-                data: updatedBookId,
-                message: 'book updated',
-            })
-        } catch (error) {
-            next(error)
+    router.put(
+        '/:bookId',
+        validationHandler({ bookId: bookIdSchema }),
+        validationHandler(updateBookSchema),
+        async function (req, res, next) {
+            const { bookId } = req.params
+            const { body: book } = req
+            try {
+                const updatedBookId = await booksService.updateBook({
+                    bookId,
+                    book,
+                })
+                res.status(200).json({
+                    data: updatedBookId,
+                    message: 'book updated',
+                })
+            } catch (error) {
+                next(error)
+            }
         }
-    })
+    )
 
-    router.delete('/:bookId', async function (req, res, next) {
-        const { bookId } = req.params
-        try {
-            const deletedBookId = await booksService.deleteBook({
-                bookId,
-            })
-            res.status(200).json({
-                data: deletedBookId,
-                message: 'books listed',
-            })
-        } catch (error) {
-            next(error)
+    router.delete(
+        '/:bookId',
+        validationHandler({ bookId: bookIdSchema }),
+        async function (req, res, next) {
+            const { bookId } = req.params
+            try {
+                const deletedBookId = await booksService.deleteBook({
+                    bookId,
+                })
+                res.status(200).json({
+                    data: deletedBookId,
+                    message: 'books listed',
+                })
+            } catch (error) {
+                next(error)
+            }
         }
-    })
+    )
 }
 
 module.exports = booksApi
